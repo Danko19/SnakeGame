5739ade790245b6cef4cacdf4afd7889bf96c14d
Danko <wally19@bk.ru>
2020-04-24 20:01:52 +0400

    RegisterPage

--- SnakeGame.Client/MainWindow.xaml.cs
+++ SnakeGame.Client/MainWindow.xaml.cs
@@ -1,42 +1,51 @@
-﻿namespace SnakeGame.Client
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace SnakeGame.Client
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private const int port = 32228;
         private readonly PlaygroundDrawer playgroundDrawer;
 
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
             playgroundDrawer = new PlaygroundDrawer(Playground);
         }
 
-        // private void ButtonBase_OnClick(object sender, RoutedEventArgs es)
-        // {
-        //     try
-        //     {
-        //         var port = 32228;
-        //         var client = new TcpClient("127.0.0.1", port);
-        //
-        //         using (var writer = new StreamWriter(client.GetStream()))
-        //         {
-        //             writer.Write(TextBox.Text);
-        //         }
-        //         client.Close();
-        //     }
-        //     catch (ArgumentNullException e)
-        //     {
-        //         Console.WriteLine("ArgumentNullException: {0}", e);
-        //     }
-        //     catch (SocketException e)
-        //     {
-        //         Console.WriteLine("SocketException: {0}", e);
-        //     }
-        //
-        //     Console.WriteLine("\n Press Enter to continue...");
-        //     Console.Read();
-        // }
+        public string Ip { get; set; } = "";
+        public string Nickname { get; set; } = "";
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs es)
+        {
+            try
+            {
+                var client = new TcpClient(Ip, port);
+        
+                using (var writer = new StreamWriter(client.GetStream()))
+                {
+                    writer.Write(Nickname);
+                }
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
     }
 }
