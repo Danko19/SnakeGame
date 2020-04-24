bf4efc4e815cd98b1fe59498780ec0b0d533fd9d
Daniil Mardanov <d.mardanov@skbkontur.ru>
2020-04-24 17:07:19 +0400

    PlaygroundDrawer

--- SnakeGame.Client/MainWindow.xaml.cs
+++ SnakeGame.Client/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Net.Sockets;
-using System.Windows;
+﻿using SnakeGame.Domain;
 
 namespace SnakeGame.Client
 {
@@ -10,35 +7,39 @@ namespace SnakeGame.Client
     /// </summary>
     public partial class MainWindow
     {
+        private readonly PlaygroundDrawer playgroundDrawer;
+
         public MainWindow()
         {
             InitializeComponent();
+            playgroundDrawer = new PlaygroundDrawer(Playground);
+            playgroundDrawer.Show(new Map(100, 50));
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs es)
-        {
-            try
-            {
-                var port = 32228;
-                var client = new TcpClient("127.0.0.1", port);
-
-                using (var writer = new StreamWriter(client.GetStream()))
-                {
-                    writer.Write(TextBox.Text);
-                }
-                client.Close();
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine("ArgumentNullException: {0}", e);
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-
-            Console.WriteLine("\n Press Enter to continue...");
-            Console.Read();
-        }
+        // private void ButtonBase_OnClick(object sender, RoutedEventArgs es)
+        // {
+        //     try
+        //     {
+        //         var port = 32228;
+        //         var client = new TcpClient("127.0.0.1", port);
+        //
+        //         using (var writer = new StreamWriter(client.GetStream()))
+        //         {
+        //             writer.Write(TextBox.Text);
+        //         }
+        //         client.Close();
+        //     }
+        //     catch (ArgumentNullException e)
+        //     {
+        //         Console.WriteLine("ArgumentNullException: {0}", e);
+        //     }
+        //     catch (SocketException e)
+        //     {
+        //         Console.WriteLine("SocketException: {0}", e);
+        //     }
+        //
+        //     Console.WriteLine("\n Press Enter to continue...");
+        //     Console.Read();
+        // }
     }
 }
---
+++ SnakeGame.Client/PlaygroundDrawer.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using SnakeGame.Domain;
+
+namespace SnakeGame.Client
+{
+    public class PlaygroundDrawer
+    {
+        private readonly Canvas canvas;
+
+        public PlaygroundDrawer(Canvas canvas)
+        {
+            this.canvas = canvas;
+        }
+
+        public void Show(Map map)
+        {
+            canvas.Visibility = Visibility.Visible;
+            var stepX = canvas.Width / map.Width;
+            var stepY = canvas.Height / map.Height;
+            for (var x = 0; x <= map.Width; x++)
+            {
+                var markupLine = CreateMarkupLine(x * stepX, x * stepX, 0, canvas.Height);
+                canvas.Children.Add(markupLine);
+            }
+
+            for (var y = 0; y <= map.Height; y++)
+            {
+                var markupLine = CreateMarkupLine(0, canvas.Width, y * stepY, y * stepY);
+                canvas.Children.Add(markupLine);
+            }
+        }
+
+        private Line CreateMarkupLine(double x1, double x2, double y1, double y2)
+        {
+            return new Line
+            {
+                X1 = x1,
+                X2 = x2,
+                Y1 = y1,
+                Y2 = y2,
+                Stroke = Brushes.DimGray,
+                StrokeThickness = 1
+            };
+        }
+    }
+}
